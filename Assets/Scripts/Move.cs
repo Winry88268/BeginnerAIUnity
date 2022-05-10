41d4331c0fd9b97e110c68c81e81f524977e386e
Naea Fairbrother <NaeaFairbrother@gmail.com>
2022-05-10 13:29:49 +0100

    Vectors & Moving in a Straight Line

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour 
+{
+    public Vector3 goal = new Vector3(5, 0, 4);
+    public float speed = 0.1f;
+
+    void Start() 
+    {
+        
+    }
+
+    private void Update() 
+    {
+        this.transform.Translate(goal.normalized * speed * Time.deltaTime);
+    }
+}
